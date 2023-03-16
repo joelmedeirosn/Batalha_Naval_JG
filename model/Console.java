26a5964186c0e3346b8c26478f3c62934cc2ff40
Gabriel Calabrese <gabrielcalabrese12345@gmail.com>
2023-03-15 21:05:40 -0300

    Dissolução do metodo defender.

--- model/Console.java
+++ model/Console.java
@@ -6,11 +6,11 @@ public class Console {
         Player player2 = new Player();
         //player1.setPlayerName();
         //player2.setPlayerName();
-        Tabuleiro gridP1 = new Tabuleiro(player1,4,3,2,1,0);
-        Tabuleiro gridP2 = new Tabuleiro(player2,4,3,2,1,0); //avioes definido como zero pois falta implementa-lo.
+        Tabuleiro gridP1 = new Tabuleiro(player1,1,1,1,1,0);
+        Tabuleiro gridP2 = new Tabuleiro(player2,1,1,1,1,0); //avioes definido como zero pois falta implementa-lo.
         gridP1.defender(gridP1);
-        gridP2.defender(gridP2);
-        gridP1.atacarNavio(gridP2);
+        //gridP2.defender(gridP2);
+        //gridP1.atacarNavio(gridP2);
         gridP2.atacarNavio(gridP1);
     }
     //falta implementar o navio porta avioes para printar no tabuleiro.
--- model/NomeInvalidoException.java
+++ model/NomeInvalidoException.java
@@ -2,6 +2,7 @@ package model;
 
 public class NomeInvalidoException extends Exception{
     public NomeInvalidoException(){
+
         super("Digite um nome entre 3 e 15 caracteres.");
     }
 }
